151bb45fe2e1f90fc5bcea403f83ea1be59579a1
Saevar Berg <saevar1990@hotmail.com>
2014-10-15 13:59:02 +0200

    fixed runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -7,7 +7,7 @@ for s in $SIZES; do
     for n in $(seq 1 20); do
         FILENAME="random$s-$n"
         echo "Sorting $FILENAME"
-        /usr/bin/time -f "%U" ./sorter $FILENAME > result
+        ./sorter $FILENAME > result
         sort -nc result
     done
 done

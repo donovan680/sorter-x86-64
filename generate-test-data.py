6b6114a0083af561bdd758e57cc2bfd67508e11b
Saevar Berg <saevar1990@hotmail.com>
2014-10-25 22:04:56 +0200

    Added max size param to test data generator.

--- generate-test-data.py
+++ generate-test-data.py
@@ -1,7 +1,9 @@
 import random
 
 
-sizes = [10, 100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]
+sizes = [10, 100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000,
+        10000000]
+maxSize = 2**64
 
 for s in sizes:
     for n in range(20):
@@ -9,5 +11,5 @@ for s in sizes:
         print ("Generating", filename)
         with open(filename, "wb", 0) as f:
             for i in range(s):
-                r = random.randint(1, 10000000000)
+                r = random.randint(1, maxSize)
                 f.write(bytes(str(r) + "\n", "UTF-8") )

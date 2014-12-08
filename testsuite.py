d8a5b399fe3bd589398c80b6c9ca5a0802b454e6
Saevar Berg <saevar1990@hotmail.com>
2014-12-08 02:35:45 +0100

    Merged generate-test-data.py into testsuite.py.

--- testsuite.py
+++ testsuite.py
@@ -6,42 +6,15 @@ sizes = [10, 100, 1000, 5000, 10000, 50000 , 100000, 500000, 1000000]
 
 #sizes = [100000, 500000, 1000000]
 
-cmpCount = {
-    10: 409
-    , 100: 634
-    , 1000: 2884
-    , 5000: 12884
-    , 10000: 25384
-    , 50000: 125384
-    , 100000: 250384
-    , 500000: 1250384
-    , 1000000: 2500384
-}
-allTimes = []
-for s in sizes:
-    times = []
-    print(str(s) + "_time: ", end="", flush=True)
-    for i in range(20):
-        testfile = "random{}-{}".format(s, i + 1)
-        command = "/usr/bin/time --format %e ./sorter {} > result".format(testfile)
-        if len(sys.argv) > 1:
-            command = "/usr/bin/time --format %e ./sorter {} 0 > result".format(testfile)
-        p = subprocess.Popen(
-                command
-                , shell=True
-                , universal_newlines=True
-                , stderr=subprocess.PIPE
-                )
-        stdout, stderr = p.communicate()
-        time = float(stderr)
-        print(str(time), end="\t", flush=True)
-        times.append(time)
-    print()
-    print(str(s) + "_mcips: ", end="", flush=True)
-    for t in times:
-        print(str(int(cmpCount[s]/(t + 0.01))), end="\t", flush=True)
-    print()
-    allTimes.append(times)
+def generateTestData():
+    for s in sizes:
+        for n in range(numSamples):
+            filename = "random" + str(s) + "-" + str(n+1)
+            print ("Generating", filename)
+            with open(filename, "wb", 0) as f:
+                for i in range(s):
+                    r = random.randint(1, maxSize)
+                    f.write(bytes(str(r) + "\n", "UTF-8"))
 
 for s in sizes:
     for t in ["asc", "desc"]:
